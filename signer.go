3ef2e1d0e0b5a383585b5d150dfd17389d328933
Vitold Komorovski <komorowskiws@gmail.com>
2020-04-19 14:06:16 +0300

    working linear variant

--- signer.go
+++ signer.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"time"
 	"sync"
+	"strings"
+	// "runtime"
 )
 
 type defaultSort []string
@@ -17,12 +19,15 @@ func (a defaultSort) Less(i, j int) bool { return a[i] < a[j] }
 
 // SingleHash func
 //func SingleHash(data string, sh chan<- string) {
-func SingleHash(data string) string {
+func SingleHash(data string, sh chan<- string) {
+	mu := &sync.Mutex{}
+	mu.Lock()
 	md := DataSignerMd5(data)
 	l :=  DataSignerCrc32(data)
 	r :=  DataSignerCrc32(md)
 	res := l + "~" + r
-	return res
+	mu.Unlock()
+	sh <- res
 }
 
 // MultiHash func
@@ -30,8 +35,11 @@ func MultiHash(data string, results chan string) {
 	var wg sync.WaitGroup 
 	wg.Add(6)      
 	multiHash := ""
+	hashed := ""
 	ch := make(chan string, 100)
-	hashed := SingleHash(data)
+	sch := make(chan string)
+	go SingleHash(data, sch)
+	hashed = <- sch
 	iterHash := func (item int, hashed string, ch chan<- string)  {
 		defer wg.Done()
 		ch <-  DataSignerCrc32(strconv.Itoa(item) + hashed)
@@ -46,14 +54,14 @@ func MultiHash(data string, results chan string) {
 }
 
 // CombineResults func
-func CombineResults(data []string) string {
+func CombineResults(data []string, out chan interface{}) {
 	go sort.Sort(defaultSort(data))
-	r := ""
-	for i := 0; i < len(data)-1; i++ {
+	r := strings.Join(data, "_")
+	/* for i := 0; i < len(data)-1; i++ {
 		r += data[i] + "_"
 	}
-	r += data[len(data)-1]
-	return r
+	r += data[len(data)-1] */
+	out <- r
 }
 
 
@@ -61,13 +69,14 @@ func CombineResults(data []string) string {
 func ExecutePipeline(data []int) {
 	var results []string
 	ch := make(chan string, 100)
+	out := make(chan interface{}, 100)
 	start := time.Now()
 	for _, i := range data {
 		go MultiHash(strconv.Itoa(i), ch)
 		results = append(results, <-ch)
-		/* runtime.Gosched() */
+		// runtime.Gosched()
 	}
-	h := CombineResults(results)
+	CombineResults(results, out)
 	t := time.Now()
 	elapsed := t.Sub(start)
 	fmt.Println(elapsed)
@@ -75,7 +84,7 @@ func ExecutePipeline(data []int) {
 }
 
 func main() {
-	//inputData := []int{0, 1, 1, 2, 3, 5, 8}
-	inputData := []int{0, 1}
+	inputData := []int{0, 1, 1, 2, 3, 5, 8}
+	//inputData := []int{0, 1}
 	ExecutePipeline(inputData)
 }
